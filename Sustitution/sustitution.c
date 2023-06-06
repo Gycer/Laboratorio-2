7fde534777ebb3d632b0a93a132c4a0f7573e766
Ghema <19-10161@usb.ve>
2023-06-06 00:16:19 -0400

    documentacion

--- Sustitution/sustitution.c
+++ Sustitution/sustitution.c
@@ -3,22 +3,29 @@
 #include <stdlib.h>
 #include <cstring>
 
+/*Programa de cifrado de texto*/
+/*Mediante sustitucion de alfabeto*/
 
 char* clave;
 char abecedario[27] = "abcdefghijklmnopqrstuvwxyz";
 char TextoACifrar[1000];
 
+/*Se valida si la letra recibida 
+ corresponde a un caracter valido*/
 int esLetra(char c){
     int esMayuscula = ( 65 <= c && c <= 90 );
     int esMinuscula = ( 97 <= c && c <= 122 );
     return esMayuscula || esMinuscula;
 }
 
+/*Se valida si la clave cumple con los parametros requeridos*/
 int ValidacionClave() {
+    
+    //El alfabeto debe contener 26 letras
     if (strlen(clave) != 26){
         return 1;
     }
-        
+    //No debe poseer caracteres repetidos
     for (int i = 0; i < 26; i++)
     {
         for (int j = i+1; j < 27; j++)
@@ -35,12 +42,13 @@ int ValidacionClave() {
     return 0;
 }
 
+/*Se codifica el mensaje*/
 void codificar(){
    
     printf("Ciphertext: ");
-
     int n = strlen(TextoACifrar);
-
+   
+    //Se revisa letra por letra el texto
     for (int i = 0; i < n; i++)
     {
         
@@ -48,6 +56,7 @@ void codificar(){
        int esMayuscula = (65 <= letra && letra <= 90);
        int posicion = -1;
 
+        //Se localiza el indice de la letra leida
        for (int j = 0; j < 27; j++)
        {
             if (abecedario[j] == tolower(letra)){
@@ -55,9 +64,12 @@ void codificar(){
                 break;
             }
        }
+        //Si es un caracter ajeno al alfabeto se imprime sin modificacion
         if (posicion == -1){
             printf("%c", letra);
         }
+        
+        //Si es mayuscula se imprime en mayuscula
        else if(esMayuscula){
         printf("%c", toupper(clave[posicion]));
        }
@@ -70,6 +82,8 @@ void codificar(){
 
 int main(int argc, char *argv[]){
 
+    //Se valida la cantidad de argumentos
+    //que se recibe en la linea de comandos
     if (argc != 2)
     {
         return 1;
@@ -78,6 +92,7 @@ int main(int argc, char *argv[]){
     printf("\n\n→→→→ BIENVENIDOS A \"1N¢R1PT4¢10N\"←←←←\n\n");
     clave = argv[1];
 
+    
     if (ValidacionClave() == 1){
         printf("Error en la clave suministrada\n");
         return 1;    
